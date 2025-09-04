dd437408eb6ed50f23d6329be302dc39ff1186ca
sssctkt <justaskaisssctkt@gmail.com>
2025-09-04 12:26:31 -0400

    first program

--- introduction/01_introduction.py
+++ introduction/01_introduction.py
@@ -1,1 +1,14 @@
-#first program here
+#first program in python
+#you have to program in a format that they understand
+
+#comment
+
+#keyword
+#print (output)
+#you need opening and closing quotation
+#message needs to be inside the opening and closing quotation
+print("Hello!!") #statement
+print("This is my next sentence")
+
+#another print statement will go to the next line
+
