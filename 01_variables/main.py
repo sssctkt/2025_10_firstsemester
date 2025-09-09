fa54c7205aed0b0a27f1884db7dac1633b42b06d
sssctkt <justaskaisssctkt@gmail.com>
2025-09-09 13:58:09 -0400

    rules on creating variable (ex 2 q3 answer)

--- 01_variables/main.py
+++ 01_variables/main.py
@@ -1,5 +1,23 @@
 #variable--to store and manipulate the value
 # = (assign value from RIGHT TO LEFT)
 #example
-number=2.1
-print("The number is:",number)
+#rules on creating variables
+#2.1=number (wrong)
+
+#1. it has to be one phrase
+zoe_and_kirpa=34
+seth_and_ali=17
+#2. number cannot go first
+#3x=21
+x3=21
+print(x3)
+#3. Do not use keyword as your variable name
+#print=45
+#print(print) NO
+
+#below is the suggestion on how to create a good
+#variable name
+
+#1. try to keep it in lowercase
+#2. instead of creating something like x1234=25
+#be more indicative radius=25
