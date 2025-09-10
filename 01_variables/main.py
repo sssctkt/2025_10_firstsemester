d39c5565e493154d9be72325c9f012675fe6f2bd
sssctkt <justaskaisssctkt@gmail.com>
2025-09-10 12:50:06 -0400

    swapping variables

--- 01_variables/main.py
+++ 01_variables/main.py
@@ -1,3 +1,21 @@
-temp=float(input("Enter the temperature in Celsius: "))
-temp=temp*1.8+32
-print("The temperature in Fahrenheit is:",temp)
+#task, is to swap variables
+#you cannot change what I write in here
+#but you can add more
+
+x=(input("Enter your first message: "))
+y=(input("Enter your second message: "))
+
+
+#do your work here
+#you can create one more variable, but only one extra
+z=x
+x=y
+y=z
+
+
+
+#output
+print(x) #the second number
+print(y) #the first number
+
+
