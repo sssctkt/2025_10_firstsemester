7edc0dcaaad4770349dbdf609c5551bcd1595354
sssctkt <justaskaisssctkt@gmail.com>
2025-09-11 13:44:47 -0400

    exercise 2 q5 answer

--- 01_variables/main.py
+++ 01_variables/main.py
@@ -1,20 +1,16 @@
-#Can a variable in python change its storing type?
-
-#short answer: yes
-
-number=123
-number=number+4
-print(number)
+#What is an assignment symbol? Give one example of correct assignment,
+#and one example of incorrect assignment
 
 
-number="this is a sentence!"
-number=number+3
+"""
+= is an assignment symbol. It assign values from right to left
+correct example:
+x=24
+incorrect example
 
-print(number)
 
-#long answer: while you can change change of a variable
-#please (at least in class) do not do it
-#because you are making it way more confusing in doing so
 
-#what if you need another type?? Simply create another variable
-#while the value of the variable can be changed. Try not to change the type
+
+
+
+"""
