9c5b0986b1679865b6362ac82d2a17ace52c8648
sssctkt <justaskaisssctkt@gmail.com>
2025-09-11 14:23:20 -0400

    math operators

---
+++ 02_operators/math_operators/main.py
@@ -0,0 +1,12 @@
+#math operator
+# + --> addition
+# - --> subtraction
+# * --> mulitiplication
+# / --> division
+#bedmas applies
+# % modulus a%b will give you the remainder
+# (in this class, % will be used with positive integer only)
+# // speicial kind of division, it will round DOWN and give you an integer
+# ** exponent
+
+print(2*3**2)
