7a16b24472ba2597decfdee68e479f548f3eb743
sssctkt <justaskaisssctkt@gmail.com>
2025-10-01 13:45:10 -0400

    if else statement

---
+++ 03_if_else_statement/main.py
@@ -0,0 +1,11 @@
+#if  else statement
+
+number=int(input("Enter an integer: "))
+
+if (number%4==0):
+    #everything is true under this indentation
+    print("The number is mulitple of 4")
+    print("I see ppl playing minesweeper!!!")
+
+print("This is not under if block")
+    
