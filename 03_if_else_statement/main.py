67571978e19c93f78c63743e80dc869742c1341c
sssctkt <justaskaisssctkt@gmail.com>
2025-10-01 13:52:40 -0400

    introducing else

--- 03_if_else_statement/main.py
+++ 03_if_else_statement/main.py
@@ -7,5 +7,8 @@ if (number%4==0):
     print("The number is mulitple of 4")
     print("I see ppl playing minesweeper!!!")
 
+else:
+    print("This is not a multiple of 4")
+
 print("This is not under if block")
-    
+#can someone tell me why this last line will appear all the time  
