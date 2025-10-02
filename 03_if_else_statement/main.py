cb35c19c252836c01c5e32a76caa59d01ff4767a
sssctkt <justaskaisssctkt@gmail.com>
2025-10-02 12:35:18 -0400

    nested if statement

--- 03_if_else_statement/main.py
+++ 03_if_else_statement/main.py
@@ -4,22 +4,39 @@
 #1. if elif else
 #2. nested if statement
 
-#what happens if you have more than 2 choices
-
+#nested if statement-if statements inside an if statements
+number=int(input("Enter a positive integer: "))
 color =input ("Enter your favorite color, red, yellow, blue, or pink: ")
-if (color=="red"):
-    print("You chose the color red")
-    print("You like english class?")
-elif (color=="yellow"):
-    print("You chose the color yellow")
-    print("You like music??")
-elif (color=="blue"):
-    print("You chose the color blue")
-    print("You like chess??")
+if (number%2==0):
+    print("You chose an even number!!")
+    print("you must like barcelona!!")
+    if (color=="red"):
+        print("You chose the color red")
+        print("You like english class?")
+    elif (color=="yellow"):
+        print("You chose the color yellow")
+        print("You like music??")
+    elif (color=="blue"):
+        print("You chose the color blue")
+        print("You like chess??")
+    else:
+        print("You chose the color pink")
+        print("You like pineapple pizza")
 else:
-    print("You chose the color pink")
-    print("You like pineapple pizza")
-
+    print("you chose an odd number!!")
+    print("You must like walking!")
+    if (color=="red"):
+        print("You chose the color red")
+        print("You like english class?")
+    elif (color=="yellow"):
+        print("You chose the color yellow")
+        print("You like music??")
+    elif (color=="blue"):
+        print("You chose the color blue")
+        print("You like chess??")
+    else:
+        print("You chose the color pink")
+        print("You like pineapple pizza")
 
 
 
