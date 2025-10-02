d393f82fc0dacd37398842ddae2700358b209e1b
sssctkt <justaskaisssctkt@gmail.com>
2025-10-02 12:26:34 -0400

    if elif and else

--- 03_if_else_statement/main.py
+++ 03_if_else_statement/main.py
@@ -1,14 +1,27 @@
-#if  else statement
+#if else statement
 
-number=int(input("Enter an integer: "))
+#2 more forms of if else statement
+#1. if elif else
+#2. nested if statement
 
-if (number%4==0):
-    #everything is true under this indentation
-    print("The number is mulitple of 4")
-    print("I see ppl playing minesweeper!!!")
+#what happens if you have more than 2 choices
 
+color =input ("Enter your favorite color, red, yellow, blue, or pink: ")
+if (color=="red"):
+    print("You chose the color red")
+    print("You like english class?")
+elif (color=="yellow"):
+    print("You chose the color yellow")
+    print("You like music??")
+elif (color=="blue"):
+    print("You chose the color blue")
+    print("You like chess??")
 else:
-    print("This is not a multiple of 4")
+    print("You chose the color pink")
+    print("You like pineapple pizza")
 
-print("This is not under if block")
-#can someone tell me why this last line will appear all the time  
+
+
+
+
+          
