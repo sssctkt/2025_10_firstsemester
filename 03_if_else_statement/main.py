013f7f42711864603bff79f72c107747ed7d7a47
sssctkt <justaskaisssctkt@gmail.com>
2025-10-06 13:04:40 -0400

    more confusion of if else statements

--- 03_if_else_statement/main.py
+++ 03_if_else_statement/main.py
@@ -1,15 +1,15 @@
-#logical confusion
-#why do we need elif and else??
+grade=int(input("Enter the grade"))
 
-#please remember, elif and else, you pick one and discards the others
+if (grade>=90 and grade<=100):
+    print("A")
+if (grade >=80 and grade <=90):
+    print("B")
+if (grade>=70 and grade <=80):
+    print("C")
+if (grade>=60 and grade <=70):
+    print("D")
+else:
+    print("you fail")
 
-#let's do a not obvious example first
-
-ph=int(input("Enter the ph level"))
-
-if (ph<=2):
-    print("it's very acidic")
-if(ph<7):
-    print("acidic")
-    
-#always use if and elif
+#if you know the correct answer when the input is 90, (or 80), then 
+#you master this concept
