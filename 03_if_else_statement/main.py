16a3cc1d6e80c201494de1701db9887de8704d9d
sssctkt <justaskaisssctkt@gmail.com>
2025-10-06 12:54:04 -0400

    if and else (logical confusion)

--- 03_if_else_statement/main.py
+++ 03_if_else_statement/main.py
@@ -1,44 +1,15 @@
-#if else statement
+#logical confusion
+#why do we need elif and else??
 
-#2 more forms of if else statement
-#1. if elif else
-#2. nested if statement
+#please remember, elif and else, you pick one and discards the others
 
-#nested if statement-if statements inside an if statements
-number=int(input("Enter a positive integer: "))
-color =input ("Enter your favorite color, red, yellow, blue, or pink: ")
-if (number%2==0):
-    print("You chose an even number!!")
-    print("you must like barcelona!!")
-    if (color=="red"):
-        print("You chose the color red")
-        print("You like english class?")
-    elif (color=="yellow"):
-        print("You chose the color yellow")
-        print("You like music??")
-    elif (color=="blue"):
-        print("You chose the color blue")
-        print("You like chess??")
-    else:
-        print("You chose the color pink")
-        print("You like pineapple pizza")
-else:
-    print("you chose an odd number!!")
-    print("You must like walking!")
-    if (color=="red"):
-        print("You chose the color red")
-        print("You like english class?")
-    elif (color=="yellow"):
-        print("You chose the color yellow")
-        print("You like music??")
-    elif (color=="blue"):
-        print("You chose the color blue")
-        print("You like chess??")
-    else:
-        print("You chose the color pink")
-        print("You like pineapple pizza")
+#let's do a not obvious example first
 
+ph=int(input("Enter the ph level"))
 
-
-
-          
+if (ph<=2):
+    print("it's very acidic")
+if(ph<7):
+    print("acidic")
+    
+#always use if and elif
