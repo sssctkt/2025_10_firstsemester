a9d499493483abab042f1c2f65356f42e29c3e30
sssctkt <justaskaisssctkt@gmail.com>
2025-10-17 14:10:28 -0400

    Create while_loop_and_for_loop.py

---
+++ 04_while_loop_and_for_loop/while_loop/while_loop_and_for_loop.py
@@ -0,0 +1,11 @@
+#loop (while loop and for loop)
+
+#why do we learn loop??
+
+#according to the structred program theorem
+#given any solvable programming questions
+
+#as long as you know these 3 things:
+#1. next step (sequence)
+#2. decision (if else statement) (selection)
+#3. loop (iteration)
