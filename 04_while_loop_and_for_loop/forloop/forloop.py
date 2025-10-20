637aa525dd7b3372418db21988dfb5c5b3d12e98
sssctkt <justaskaisssctkt@gmail.com>
2025-10-20 12:42:53 -0400

    Create forloop.py

---
+++ 04_while_loop_and_for_loop/forloop/forloop.py
@@ -0,0 +1,30 @@
+#1 start with a condition
+"""
+countdown=int(input("Enter a number to start a countdown: "))
+
+while(countdown>=0): #2 condition
+    print(countdown)
+    countdown=countdown-1 #3 create a chance for the loop to exit
+"""
+#countup=int(input("Enter a number between 1 and 10: ")) #1
+"""
+while(countup<=100): #2
+    print(countup)
+    countup=countup+2 #3
+"""
+#we have other loops called "for loop"
+#for loop and while loop are very similiar
+#you can "replace" it with each other
+
+countup=int(input("Enter a number between 1 and 10: ")) #1
+for maxim in range(countup, 101, 2): #start, end(excluding), increment
+    print(maxim)
+
+
+
+
+
+
+
+
+
