6db778e1ba9d5575329486a97438983a92d7c4ae
sssctkt <justaskaisssctkt@gmail.com>
2025-10-23 13:41:04 -0400

    while loop and break

--- 04_while_loop_and_for_loop/while_loop/while loop.py
+++ 04_while_loop_and_for_loop/while_loop/while loop.py
@@ -1,20 +1,16 @@
-#loop (while loop and for loop)
+"""
+1.difference between while loop and for loop
+2.keyword break 
+3. we can design a little game
+"""
 
-#why do we learn loop??
+#break--immediately exit the loop it is in (one level) 
+#for loop--use it when you know how many times it will loop 
+#while loop --use it if you are lookng for a specific case to get out
+print("let's play an interesting game!!!")
+while(True):
+    answer=input("Do you want to do homework?? yes or no: ")
+    if (answer=="yes"):
+        break 
 
-#according to the structred program theorem
-#given any solvable programming questions
-
-#as long as you know these 3 things:
-#1. next step (sequence)
-#2. decision (if else statement) (selection)
-#3. loop (iteration)
-
-#goal of this program
-#is to create a "FAIR" random number genenerator to select student
-import random
-number=1
-while(number!=7):
-    number=random.randint(1, 20)
-
-print("this fair ranodm generator selects the number: ",number)
+print("Then do your homework")
