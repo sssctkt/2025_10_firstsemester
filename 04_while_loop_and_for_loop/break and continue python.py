a3d71a33b780a47ea0a100ca5d7423db7a163691
sssctkt <justaskaisssctkt@gmail.com>
2025-10-24 12:44:04 -0400

    break and continue (for loop)

---
+++ 04_while_loop_and_for_loop/break and continue python.py
@@ -0,0 +1,15 @@
+#break and continue
+#the difference between while loop and for loop
+
+#break -->get out of the loop(only the loop the keyword is in)
+#continue-->go back to the control mechanism
+a=1
+while a<31:
+    if (a==13):
+        a+=1
+        continue
+    print(a)
+    a=a+1
+
+#FOR LOOP MAKE SURE THAT INCREMENT/DECREMENT IS THE LAST THING TO HAPPEN
+    
