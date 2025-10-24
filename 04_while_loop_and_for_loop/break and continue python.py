e454fd2fb065387280ac9d94bd795d6c8ad6aa87
sssctkt <justaskaisssctkt@gmail.com>
2025-10-24 12:45:12 -0400

    Update break and continue python.py

--- 04_while_loop_and_for_loop/break and continue python.py
+++ 04_while_loop_and_for_loop/break and continue python.py
@@ -3,13 +3,13 @@
 
 #break -->get out of the loop(only the loop the keyword is in)
 #continue-->go back to the control mechanism
-a=1
-while a<31:
+
+for a in range(1, 31, 1):
     if (a==13):
-        a+=1
         continue
     print(a)
-    a=a+1
+    
 
 #FOR LOOP MAKE SURE THAT INCREMENT/DECREMENT IS THE LAST THING TO HAPPEN
     
+
