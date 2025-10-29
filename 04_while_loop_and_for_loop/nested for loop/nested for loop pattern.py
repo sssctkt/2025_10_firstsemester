22f46e1cf245e5f76742142db68af60018149741
sssctkt <justaskaisssctkt@gmail.com>
2025-10-29 13:46:33 -0400

    Update nested for loop pattern.py (another pattern)

--- 04_while_loop_and_for_loop/nested for loop/nested for loop pattern.py
+++ 04_while_loop_and_for_loop/nested for loop/nested for loop pattern.py
@@ -2,6 +2,6 @@
 
 row=int(input("Enter the number of row: "))
 for a in range(1, row+1, 1):
-    for b in range(1, a+1, 1):
+    for b in range(row, a-1, -1):
         print("*", end="") #end="" means continue on the same line
     print() #next line
