39898ee101584a787a1095826fc0779228b22a95
sssctkt <justaskaisssctkt@gmail.com>
2025-11-10 12:34:56 -0500

    Create pre_built_function.py

---
+++ 05_pre_built_functions/pre_built_function.py
@@ -0,0 +1,34 @@
+#pre-built function
+#simple string and list methods
+#user defined function
+
+#pre-built function
+#what is a function in python? a block that does a specific task for you
+#it helps to reuse the code
+
+#example
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+c=int(input("Enter the third number: "))
+d=int(input("Enter the fourth number: "))
+"""
+instead of doing it by yourself
+if (a>b and a>c):
+    print(a)
+elif (b>a and b>c):
+    print(b)
+else:
+    print(c)
+
+you can use pre-built functions made by others
+as long as you know them
+"""
+#1. max()
+answer=max(a,b,c,d)
+print(answer)
+#please remember max(a,b,c,d) a,b,c,d-->argument/parameter
+#the whole max(a,b,c,d) will become an answer then assign to the variable
+
+
+
+    
