d66d4b347d8dd08c3e98849fb1246aa3db729930
sssctkt <justaskaisssctkt@gmail.com>
2025-11-13 14:14:53 -0500

    Create string_methods.py

---
+++ 06_strings_methods/string_methods.py
@@ -0,0 +1,17 @@
+#String methods
+
+#string methods are to help us to manipulate string easier
+#string methods all start with str.
+
+#ex str.capitalize()
+#inside the bracket, it has no arugment for this one
+#your first argument is always going to be the string
+
+word="mississauga"
+word=str.capitalize(word)
+
+print(word)
+
+#another ex str.count(s)
+number=str.count(word, "a")
+print(number)
