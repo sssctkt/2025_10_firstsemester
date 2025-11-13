74b7a5e4965faa04b0ea40b40b573e640ef63790
sssctkt <justaskaisssctkt@gmail.com>
2025-11-13 13:41:47 -0500

    calling and returning of a function

--- 05_pre_built_functions/pre_built_function.py
+++ 05_pre_built_functions/pre_built_function.py
@@ -1,49 +1,10 @@
-#rest of the pre-built functions
-#max()--finds the maximum
-answer=max(1, 2, 3, 4, 5,2) #argument/paramater
-print(answer)
+#functions
+#max() min() round() str() abs() float() string(), etc
+a=int(input())
+b=int(input())
 
-#min()--finds the minimum
-answer=min(2, 4)
-print(answer)
+answer=max(a,b)
 
-#abs()--absolute value--it will change the negative value into positive
-answer1=abs(-6)
-answer2=abs(8)
-print(answer1, answer2)
-
-#int()--change it into integer
-answer=int(7.999)
-print(answer)
-
-#float() --change it into a float number (decimal)
-answer=float(10)
-print(answer)
-
-#str()--change it into string
-print(9*3)
-answer=str(9)
-print(answer*3)
-
-#len()--to find the length of a sentence (doesn't work on numbers)
-sentence="Daniel, it's okay!! Let's take it easy!"
-answer=len("Daniel, it's okay!! Let's take it easy!")
-print(answer)
-print(sentence[7])
-
-#round()--round it into an integer, if you have only one parameter
-answer=round(6.8)
-print(answer)
-#if you put a comma in between, it will round to the second's number decimal
-#place
-answer=round(1.23456, 4)
-print(answer)
-
-
-
-
-
-
-
-
-
+#1. when you have max(a,b), you are CALLING the function
+#2 when the whole process is done,the whole max(a,b) will RETURN the value
+#and in this case, it assigns back to the variable named answer
