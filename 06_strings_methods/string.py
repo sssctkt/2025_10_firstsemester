c171169052b10f340da6ba1bfc9e9fc55aa47c2b
sssctkt <justaskaisssctkt@gmail.com>
2025-11-13 14:00:40 -0500

    two attributes of strings

--- 06_strings_methods/string.py
+++ 06_strings_methods/string.py
@@ -1,20 +1,15 @@
-#string methods
-#what are methods--methods are very familiar to functions, but they help
-#you to manipulate strings
+#string
 
-#before we talk about string methods
-#let's talk about strings
+#1 index and content--index always starts at zero
+#2 strings are immutable in python
 
-#what are strings--collection of characters
-#for example
-sentence="icd2O0 class!!"
-#what is the length of this sentence?
-#length starts at 1
-print(len(sentence))
-print(sentence[1])
-print(sentence[13])
-for i in range(1, len(sentence), 2):
-    print(sentence[i], end="")
+sentence="max"
+sentence[2]="n"
+print(sentence) #you CANNOT do this in python
+
+
+
+
 
 
 
