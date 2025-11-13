3d9ba47a7047091e86d1b6dc87570ce02b81b9f3
sssctkt <justaskaisssctkt@gmail.com>
2025-11-13 13:56:45 -0500

    basic of string

---
+++ 06_strings_methods/string.py
@@ -0,0 +1,23 @@
+#string methods
+#what are methods--methods are very familiar to functions, but they help
+#you to manipulate strings
+
+#before we talk about string methods
+#let's talk about strings
+
+#what are strings--collection of characters
+#for example
+sentence="icd2O0 class!!"
+#what is the length of this sentence?
+#length starts at 1
+print(len(sentence))
+print(sentence[1])
+print(sentence[13])
+for i in range(1, len(sentence), 2):
+    print(sentence[i], end="")
+
+
+
+
+
+
