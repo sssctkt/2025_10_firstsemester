88648817c178762e8f439fe33e1265d6347523d8
sssctkt <justaskaisssctkt@gmail.com>
2025-11-20 12:25:17 -0500

    Create list.py

---
+++ 07_list_methods/list.py
@@ -0,0 +1,18 @@
+#what is a list?
+
+#list is a keyword-->it can collect data from different
+#purpose is to make coding easier and more organiszed
+
+a_list=["Kwan", "compsci", 40]
+
+print(a_list)
+
+#similiarity between string and list
+
+sentence="Neil can't go wrong"
+print(sentence[5])
+
+print(a_list[1])#like string, the index starts at 0
+print(len(sentence))
+print(len(a_list))#like string,the len starts at 1
+#len() function works for both string and list
